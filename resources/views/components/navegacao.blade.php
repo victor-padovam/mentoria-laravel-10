c0f292bb9289ebe3363d788c49c87388b86efab0
Victor Padovam <victorpadovam1997@gmail.com>
2023-05-11 15:31:08 -0300

    front-end

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,31 @@
+ <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
+     <div class="position-sticky pt-3 sidebar-sticky">
+         <ul class="nav flex-column">
+             <li class="nav-item">
+                 <a class="nav-link active" aria-current="page" href="#">
+                     <span data-feather="home" class="align-text-bottom"></span>
+                     Dashboard
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="file" class="align-text-bottom"></span>
+                     Venda
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                     Produto
+                 </a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">
+                     <span data-feather="users" class="align-text-bottom"></span>
+                     Clientes
+                 </a>
+             </li>
+
+         </ul>
+     </div>
+ </nav>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.111.3">
+    <title>Gestao</title>
+
+    @yield('styles')
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href=" {{ asset('icones/apple-touch-icon.png') }}" sizes="180x180">
+    <link rel="icon" href=" {{ asset('icones/favicon-32x32.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href=" {{ asset('icones/favicon-16x16.png') }}" sizes="16x16" type="image/png">
+    <link rel="manifest" href="{{ asset('icones/manifest.json') }}">
+    <link rel="mask-icon" href="{{ asset('icones/safari-pinned-tab.svg') }}" color="#712cf9">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <meta name="theme-color" content="#712cf9">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .b-example-divider {
+            width: 100%;
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
+
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
+
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
+
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
+
+        .btn-bd-primary {
+            --bd-violet-bg: #712cf9;
+            --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+            --bs-btn-font-weight: 600;
+            --bs-btn-color: var(--bs-white);
+            --bs-btn-bg: var(--bd-violet-bg);
+            --bs-btn-border-color: var(--bd-violet-bg);
+            --bs-btn-hover-color: var(--bs-white);
+            --bs-btn-hover-bg: #6528e0;
+            --bs-btn-hover-border-color: #6528e0;
+            --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+            --bs-btn-active-color: var(--bs-btn-hover-color);
+            --bs-btn-active-bg: #5a23c8;
+            --bs-btn-active-border-color: #5a23c8;
+        }
+
+        .bd-mode-toggle {
+            z-index: 1500;
+        }
+    </style>
+
+</head>
+
+<body>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">GESTAO</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
+            aria-label="Search">
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">SAIR</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            @include('components.navegacao')
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+                <div
+                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Dashboard</h1>
+                </div>
+
+
+                {{-- <h2>Section title</h2>
+                <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Header</th>
+                                <th scope="col">Header</th>
+                                <th scope="col">Header</th>
+                                <th scope="col">Header</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1,001</td>
+                                <td>random</td>
+                                <td>data</td>
+                                <td>placeholder</td>
+                                <td>text</td>
+                            </tr>
+                            <tr>
+                                <td>1,002</td>
+                                <td>placeholder</td>
+                                <td>irrelevant</td>
+                                <td>visual</td>
+                                <td>layout</td>
+                            </tr>
+                            <tr>
+                                <td>1,003</td>
+                                <td>data</td>
+                                <td>rich</td>
+                                <td>dashboard</td>
+                                <td>tabular</td>
+                            </tr>
+                            <tr>
+                                <td>1,003</td>
+                                <td>information</td>
+                                <td>placeholder</td>
+                                <td>illustrative</td>
+                                <td>data</td>
+                            </tr>
+                            <tr>
+                                <td>1,004</td>
+                                <td>text</td>
+                                <td>random</td>
+                                <td>layout</td>
+                                <td>dashboard</td>
+                            </tr>
+                            <tr>
+                                <td>1,005</td>
+                                <td>dashboard</td>
+                                <td>irrelevant</td>
+                                <td>text</td>
+                                <td>placeholder</td>
+                            </tr>
+                            <tr>
+                                <td>1,006</td>
+                                <td>dashboard</td>
+                                <td>illustrative</td>
+                                <td>rich</td>
+                                <td>data</td>
+                            </tr>
+                            <tr>
+                                <td>1,007</td>
+                                <td>placeholder</td>
+                                <td>tabular</td>
+                                <td>information</td>
+                                <td>irrelevant</td>
+                            </tr>
+                            <tr>
+                                <td>1,008</td>
+                                <td>random</td>
+                                <td>data</td>
+                                <td>placeholder</td>
+                                <td>text</td>
+                            </tr>
+                            <tr>
+                                <td>1,009</td>
+                                <td>placeholder</td>
+                                <td>irrelevant</td>
+                                <td>visual</td>
+                                <td>layout</td>
+                            </tr>
+                            <tr>
+                                <td>1,010</td>
+                                <td>data</td>
+                                <td>rich</td>
+                                <td>dashboard</td>
+                                <td>tabular</td>
+                            </tr>
+                            <tr>
+                                <td>1,011</td>
+                                <td>information</td>
+                                <td>placeholder</td>
+                                <td>illustrative</td>
+                                <td>data</td>
+                            </tr>
+                            <tr>
+                                <td>1,012</td>
+                                <td>text</td>
+                                <td>placeholder</td>
+                                <td>layout</td>
+                                <td>dashboard</td>
+                            </tr>
+                            <tr>
+                                <td>1,013</td>
+                                <td>dashboard</td>
+                                <td>irrelevant</td>
+                                <td>text</td>
+                                <td>visual</td>
+                            </tr>
+                            <tr>
+                                <td>1,014</td>
+                                <td>dashboard</td>
+                                <td>illustrative</td>
+                                <td>rich</td>
+                                <td>data</td>
+                            </tr>
+                            <tr>
+                                <td>1,015</td>
+                                <td>random</td>
+                                <td>tabular</td>
+                                <td>information</td>
+                                <td>text</td>
+                            </tr>
+                        </tbody>
+                    </table> --}}
+        </div>
+        </main>
+    </div>
+    </div>
+
+    @yield('scripts')
+    <script src="/js/bootstrap.blundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js"></script>
+    <script src="/js/dashboard.js"></script>
+    <script src="/js/color-modes.js"></script>
+
+</body>
+
+</html>
